ad0f01fb017471600ff0c415bdfdc4dfae92eea3
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-07-17 17:26:09 +0200

    Add the files for resampling.

---
+++ 0_netcdf_resample.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import numpy as np
+
+# pcraster dynamic framework is used:
+from pcraster.framework import DynamicFramework
+
+# classes used in this script
+from dynamic_resample_framework import ResampleFramework
+
+# time object
+from currTimeStep import ModelTime
+
+# utility module:
+import virtualOS as vos
+
+# variable dictionaries:
+import efas_variable_list_final as varDict
+
+
+# efas_variable_code in a list
+efas_variable_name = ["pd","pr","rg","ta","ws"]
+
+# obtain efas_variable_code from the system argurment
+try:
+   efas_variable_name = sys.argv[1]
+except:
+   pass
+
+
+# starting and end dates
+startDate = "1990-01-01" # "2008-01-01" # "1990-01-01" #YYYY-MM-DD
+endDate   = "2014-10-31" # "2013-12-31" # "2014-10-31" #YYYY-MM-DD
+
+# input netcdf file:
+input_netcdf = {}
+input_netcdf['folder']           = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/2.5min/"+varDict.netcdf_short_name[efas_variable_name]+"/"
+input_netcdf['file_name']        = varDict.netcdf_short_name[efas_variable_name]+"_efas_rhine-meuse.nc"
+input_netcdf['file_name']        = input_netcdf['folder']+"/"+input_netcdf['file_name']
+input_netcdf['variable_name']    = varDict.netcdf_short_name[efas_variable_name]
+input_netcdf['clone_file']       = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/cell_area_maps/RhineMeuseCellsize2.5min.map"
+input_netcdf['cell_resolution']  = 2.5/60.
+# cell area (m2) for the input netcdf file:
+input_netcdf['cell_area']        = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/cell_area_maps/RhineMeuseCellsize2.5min.map"
+
+# output netcdf file:
+output_netcdf = {}
+# cell size/length/resolution (arc-degree) for the output netcdf file 
+output_netcdf['cell_resolution'] = 5./60.
+output_netcdf['folder']          = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/5min/"+varDict.netcdf_short_name[efas_variable_name]+"/"
+output_netcdf['file_name']       = varDict.netcdf_short_name[efas_variable_name]+"_efas_rhine-meuse.nc"
+output_netcdf['file_name']       = output_netcdf['folder']+"/"+output_netcdf['file_name']
+output_netcdf['variable_name']   = input_netcdf['variable_name']
+output_netcdf['variable_unit']   = varDict.netcdf_unit[efas_variable_name]
+output_netcdf['long_name']       = varDict.netcdf_long_name[efas_variable_name] 
+#
+# input and output resolutions at arc minute unit rounded to one value behind the decimal
+input_cell_size_in_arc_minutes  = np.round(input_netcdf['cell_resolution']  * 60.0, 1) 
+output_cell_size_in_arc_minutes = np.round(output_netcdf['cell_resolution'] * 60.0, 1) 
+important_information  = "The dataset was first resampled to "+str(input_cell_size_in_arc_minutes)+" arc minute resolution "
+important_information += "and then aggregated to "+str(input_cell_size_in_arc_minutes)+" arc minute resolution."  
+#
+output_netcdf['format']    = "NETCDF3_CLASSIC"
+output_netcdf['zlib']      = False
+output_netcdf['netcdf_attribute'] = {}
+output_netcdf['netcdf_attribute']['institution'] = "European Commission - JRC and Department of Physical Geography, Utrecht University"
+output_netcdf['netcdf_attribute']['title'      ] = "EFAS-Meteo 5km for Rhine-Meuse - resampled to "+str(output_cell_size_in_arc_minutes)+" arc minute resolution. "
+output_netcdf['netcdf_attribute']['source'     ] = "5km Gridded Meteo Database (C) European Commission - JRDC, 2014"
+output_netcdf['netcdf_attribute']['history'    ] = "The data were provided by Ad de Roo (ad.de-roo@jrc.ec.europa.eu) on 19 November 2014 and then converted by Edwin H. Sutanudjaja (E.H.Sutanudjaja@uu.nl) to netcdf. "
+output_netcdf['netcdf_attribute']['history'    ] += important_information 
+output_netcdf['netcdf_attribute']['references' ] = "Ntegeka et al., 2013. EFAS-Meteo: A European daily high-resolution gridded meteorological data set. JRC Technical Reports. doi: 10.2788/51262"
+output_netcdf['netcdf_attribute']['comment'    ] = "Please use this dataset only for Hyper-Hydro test bed experiments. " 
+output_netcdf['netcdf_attribute']['comment'    ] += "For using it and publishing it, please acknowledge its source: 5km Gridded Meteo Database (C) European Commission - JRDC, 2014 and its reference: Ntegeka et al., 2013 (doi: 10.2788/51262). "
+output_netcdf['netcdf_attribute']['comment'    ] += "The original data provided by JRC are in European ETRS projection, 5km grid; http://en.wikipedia.org/wiki/European_grid. "
+output_netcdf['netcdf_attribute']['comment'    ] += important_information
+output_netcdf['netcdf_attribute']['description'] = varDict.description[efas_variable_name]
+
+# make an output folder
+cleanOutputFolder = False
+try:
+    os.makedirs(output_netcdf['folder'])
+except:
+    if cleanOutputFolder: os.system('rm -r '+str(output_netcdf['folder'])+"/*")
+
+# make a temporary folder 
+tmpDir = output_netcdf['folder']+"/"+"tmp/"
+try:
+    os.makedirs(tmpDir)
+except:
+    os.system('rm -r '+str(output_netcdf['folder'])+"/tmp/*")
+
+def main():
+    
+    # time object
+    modelTime = ModelTime() # timeStep info: year, month, day, doy, hour, etc
+    modelTime.getStartEndTimeSteps(startDate,endDate)
+    
+    # resample netcdf
+    resampleModel = ResampleFramework(input_netcdf,\
+                                      output_netcdf,\
+                                      modelTime,\
+                                      tmpDir)
+    dynamic_framework = DynamicFramework(resampleModel, modelTime.nrOfTimeSteps)
+    dynamic_framework.setQuiet(True)
+    dynamic_framework.run()
+                                      
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ dynamic_resample_framework.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import datetime
+import calendar
+import numpy as np
+
+import pcraster as pcr
+from pcraster.framework import DynamicModel
+
+from outputNetcdf import OutputNetcdf
+import virtualOS as vos
+
+class ResampleFramework(DynamicModel):
+
+    def __init__(self, input_netcdf,\
+                       output_netcdf,\
+                       modelTime,\
+                       tmpDir = "/dev/shm/"):
+        DynamicModel.__init__(self) 
+
+        self.input_netcdf = input_netcdf
+        self.output_netcdf = output_netcdf 
+        self.tmpDir = tmpDir
+        self.modelTime = modelTime
+
+        # a dictionary contains input clone properties (based on the input netcdf file)
+        #~ self.input_clone = vos.netcdfCloneAttributes(self.input_netcdf['file_name'],\
+                                                     #~ np.round(self.input_netcdf['cell_resolution']*60.,1),\
+                                                     #~ True)
+        pcr.setclone(self.input_netcdf['clone_file'])
+        self.input_clone = {}
+        self.input_clone['cellsize'] = pcr.clone().cellSize()
+        self.input_clone['rows']     = int(pcr.clone().nrRows()) 
+        self.input_clone['cols']     = int(pcr.clone().nrCols())
+        self.input_clone['xUL']      = round(pcr.clone().west(), 2)
+        self.input_clone['yUL']      = round(pcr.clone().north(), 2)
+
+        # resampling factor: ratio between output and input resolutions
+        self.resample_factor = self.output_netcdf["cell_resolution"]/\
+                                self.input_netcdf['cell_resolution']
+        
+        # clone map 
+        if self.resample_factor > 1.0: # upscaling
+
+            # the resample factor must be a rounded value without decimal
+            self.resample_factor = round(self.resample_factor)
+            
+            # output clone properties
+            self.output_netcdf['rows'    ] = int(round(float(self.input_clone['rows'])/float(self.resample_factor))) 
+            self.output_netcdf['cols'    ] = int(round(float(self.input_clone['cols'])/float(self.resample_factor)))
+            self.output_netcdf['cellsize'] = self.output_netcdf["cell_resolution"]
+            self.output_netcdf['xUL'     ] = self.input_clone['xUL']
+            self.output_netcdf['yUL'     ] = self.input_clone['yUL']
+
+            # get the unique ids for the output resolution
+            # - use the clone for the output resolution (only for a temporary purpose)
+            pcr.setclone(self.output_netcdf['rows'    ],
+                         self.output_netcdf['cols'    ],
+                         self.output_netcdf['cellsize'],
+                         self.output_netcdf['xUL'     ],
+                         self.output_netcdf['yUL'     ])
+            # - unique_ids in a numpy object
+            cell_unique_ids = pcr.pcr2numpy(pcr.scalar(pcr.uniqueid(pcr.boolean(1.))),vos.MV)
+
+            # the remaining pcraster calculations are performed at the input resolution
+            pcr.setclone(self.input_clone['rows'    ],
+                         self.input_clone['cols'    ],
+                         self.input_clone['cellsize'],
+                         self.input_clone['xUL'     ],
+                         self.input_clone['yUL'     ])
+            
+            # clone map file 
+            self.clone_map_file = self.input_netcdf['clone_file']
+            
+            # cell unique ids in a pcraster object
+            self.unique_ids = pcr.nominal(pcr.numpy2pcr(pcr.Scalar,
+                              vos.regridData2FinerGrid(self.resample_factor,cell_unique_ids, vos.MV), vos.MV))
+
+            # cell area (m2)
+            self.cell_area = vos.readPCRmapClone(\
+                             self.input_netcdf["cell_area"],\
+                             self.clone_map_file,\
+                             self.tmpDir)
+            
+        else: # downscaling / resampling to smaller cell length
+
+            # all pcraster calculations are performed at the output resolution
+            pcr.setclone(self.output_netcdf['rows'    ],
+                         self.output_netcdf['cols'    ],
+                         self.output_netcdf['cellsize'],
+                         self.output_netcdf['xUL'     ],
+                         self.output_netcdf['yUL'     ])
+
+            # clone map file
+            self.clone_map_file = self.output_netcdf['clone_file']
+        
+        # an object for netcdf reporting
+        self.output = OutputNetcdf(self.output_netcdf)       
+        
+        # preparing the netcdf file at coarse resolution:
+        self.output.createNetCDF(self.output_netcdf['file_name'],
+                                 self.output_netcdf['variable_name'],
+                                 self.output_netcdf['variable_unit'])
+        
+    def initial(self): 
+        pass
+
+    def dynamic(self):
+        
+        # update model time using the current pcraster timestep value
+        self.modelTime.update(self.currentTimeStep())
+
+        # reading
+        data_available = True
+        if data_available:
+            input_value = vos.netcdf2PCRobjClone(ncFile  = self.input_netcdf['file_name'],
+                                                 varName = self.input_netcdf['variable_name'],
+                                                 dateInput = str(self.modelTime.fulldate),
+                                                 useDoy = None,
+                                                 cloneMapFileName = self.clone_map_file)
+            data_available = True  
+        
+        else:
+            print "No values are available for this date: "+str(self.modelTime)
+            data_available = False 
+        
+        if data_available: output_value = input_value
+
+        # upscaling
+        if data_available and self.resample_factor > 1.0:
+        
+            # upscaling using cell area
+            cell_area = pcr.ifthen(pcr.defined(output_value), self.cell_area)
+            output_value_in_pcraster = \
+                            vos.getValDivZero(\
+                            pcr.areatotal(output_value*self.cell_area, self.unique_ids),\
+                            pcr.areatotal(self.cell_area, self.unique_ids), vos.smallNumber)
+            
+            # resample to the output clone resolution 
+            output_value = vos.regridToCoarse(pcr.pcr2numpy(output_value_in_pcraster, vos.MV),
+                                              self.resample_factor, "max", vos.MV)
+
+        # reporting
+        if data_available:
+
+            # time stamp 
+            timestepPCR = self.modelTime.timeStepPCR
+            timeStamp = datetime.datetime(self.modelTime.year,\
+                                          self.modelTime.month,\
+                                          self.modelTime.day,0)
+            # write to netcdf 
+            self.output.data2NetCDF(self.output_netcdf['file_name'],\
+                                    self.output_netcdf['variable_name'],\
+                                    output_value,\
+                                    timeStamp)
+
+        # closing the file at the end of
+        if self.modelTime.isLastTimeStep(): self.output.close(self.output_netcdf['file_name'])
+
