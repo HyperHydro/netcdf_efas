44a2695b853d0efe950b285a8210cde69bf41225
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-18 10:01:58 +0200

    Correcting netcdf attributes.

--- correcting.py
+++ correcting.py
@@ -3,7 +3,7 @@ import netCDF4 as nc
 import glob
 
 # list of netcdf files 
-netcdf_files = glob.glob("/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/03min/*/*.nc")
+netcdf_files = glob.glob("/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/02.5min/*/*.nc")
 
 # correct attribute information (references)
 references = "Ntegeka et al., 2013. EFAS-Meteo: A European daily high-resolution gridded meteorological data set. JRC Technical Reports. doi: 10.2788/51262 ; Burek et al., 2013. Evaporation Pre-Processor for the LISFLOOD Water Balance and Flood Simulation Model. JRC Technical Reports. doi: 10.2788/26000 "
